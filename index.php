73f82f4ff583bfa9d5efe52e798312e167233fee
Votre Nom <amdy@dofbi0.2>
2020-06-24 13:20:32 +0000

    inscription

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 echo "teste ngay am foofoou ";
-<<<<<<< HEAD
+
 echo"dexieme teste bi ngaye am foofoou ";
 echo "Messi meilleur joueur au monde";
-
-
-
-=======
-
 echo "teste ngay am foofoou ";
 echo "teste ngay am foofoou ";
->>>>>>> 8f20c48a2288509f6579d662338b1ff64756aa4d
+
